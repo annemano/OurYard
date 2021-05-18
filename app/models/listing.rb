87bda1faef08c968d72edd0c1155bc879666a205
Jean Colombel <jeancolombel93@gmail.com>
2021-05-18 15:19:06 -0400

    Created the new and edit views for the listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,7 @@ class Listing < ApplicationRecord
 
   validates :title, :description, :address, :price_per_hour, presence: true
   validates :title, :address, uniqueness: true
-  validates :price_per_hour, :avg_rating, numericality: { only_integer: false }
+  validates :price_per_hour, numericality: { only_integer: false }
   validates :description, length: { maximum: 500,
                                     too_long: "%{count} characters is the maximum allowed" }
   validates :title, length: { maximum: 60,
