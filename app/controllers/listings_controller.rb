05a2eb39556d2f59156d00da717fc091ecf9c67f
Jean Colombel <jeancolombel93@gmail.com>
2021-05-18 12:48:32 -0400

    Edited the listings controller methods

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,21 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: %i[show edit update destroy]
 
   def index
+    @listings = Listing.all
   end
 
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listings_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -16,9 +25,26 @@ class ListingsController < ApplicationController
   end
 
   def update
+    @listing.update(listing_params)
+    if @listing.save
+      redirect_to listings_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @listing.destroy
+    redirect_to listings_path
   end
 
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:list).permit(:title, :description, :address, :price_per_hour)
+  end
 end
