415fe04b84fd2c39b86ad6c5bbb8b472663b17c9
Jean Colombel <jeancolombel93@gmail.com>
2021-05-19 12:04:12 -0400

    Added the info windows

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,7 +8,8 @@ class ListingsController < ApplicationController
     @markers = @listings.geocoded.map do |listing|
       {
         lat: listing.latitude,
-        lng: listing.longitude
+        lng: listing.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { listing: listing })
       }
     end
   end
