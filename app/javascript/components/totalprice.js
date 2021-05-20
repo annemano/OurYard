cd6ead983fd87cb628ca691569738a856999a157
Anne-Marie Nault <anne.ma@gmail.com>
2021-05-20 16:10:56 -0400

    made function work

--- app/javascript/components/totalprice.js
+++ app/javascript/components/totalprice.js
@@ -2,13 +2,16 @@ const addTotalPriceToForm = () => {
   const startDate = document.getElementById('booking_start_date');
   const endDate = document.getElementById('booking_end_date');
   const totalPrice = document.getElementById('total-price-show-page');
-  const hourlyPrice = document.getElementById('price-hour-show-page');
-  endDate.addEventListener("blur", (event) => {
+  const hourlyPrice = Number.parseFloat(document.getElementById('price-hour-show-page').innerHTML);
+  endDate.addEventListener("change", (event) => {
     totalPrice.innerHTML = "";
     const startDateTime = new Date(startDate.value);
     const endDateTime = new Date(endDate.value);
+    console.log(startDateTime)
+    console.log(endDateTime)
     const hours = (endDateTime - startDateTime) / 3600000;
-    totalPrice.innerHTML = `<p><strong>Total price: ${hours} * ${hourlyPrice}</strong></p>`;
+    console.log(hours)
+    totalPrice.innerHTML = `<p><strong>Total price: $${hours * hourlyPrice}</strong></p>`;
     });
   };
 
