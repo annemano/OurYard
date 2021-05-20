9c40b7682a832d1c50e43721dbfeb15c78b8c94b
Anne-Marie Nault <anne.ma@gmail.com>
2021-05-20 11:53:39 -0400

    modified routes to allow editing, added to view without styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :listings do
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create, :edit, :update]
   end
-  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
+  resources :bookings, only: [:index, :show, :destroy]
   get '/my_listings', to: 'listings#my_listings', as: 'my_listings'
   patch '/bookings/:id/confirm', to: 'bookings#confirm', as: 'booking_confirm'
   patch '/bookings/:id/reject', to: 'bookings#reject', as: 'booking_reject'
